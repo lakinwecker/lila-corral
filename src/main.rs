14024a04f4fe5f76cf6d4c8e8176f280aaa8eb5a
Lakin Wecker <lakin@structuredabstraction.com>
2021-12-27 06:16:51 -0700

    Finished with the hello world demo I wanted.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
 extern crate clap;
 extern crate dotenv;
+extern crate thiserror;
 extern crate pretty_env_logger;
 #[macro_use] extern crate log;
 
 use dotenv::dotenv;
 use structopt::StructOpt;
+use std::fmt;
 use axum::{
+    body,
     routing::get,
     Router,
+    response::Response,
+    http::StatusCode,
 };
+use thiserror::Error;
 
 #[derive(Debug, StructOpt, Clone)]
 struct DatabaseOpts {
@@ -32,6 +38,38 @@ struct CorralWebserverOpts {
     database_opts: DatabaseOpts,
 }
 
+#[derive(Error, Debug)]
+pub enum Error {
+    ReqwestError(#[from] reqwest::Error)
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Error::ReqwestError(err) => write!(f, "Reqwest request: {}", err),
+        }
+    }
+}
+
+impl axum::response::IntoResponse for Error {
+    fn into_response(self) -> Response {
+        Response::builder()
+            .status(StatusCode::INTERNAL_SERVER_ERROR)
+            .body(body::boxed(body::Full::from(self.to_string())))
+            .unwrap()
+    }
+}
+
+
+async fn handler() -> Result<String, Error> {
+    info!("Some logging");
+    let body = reqwest::get("https://wttr.in")
+        .await?
+        .text()
+        .await?;
+    Ok(body)
+}
+
 #[tokio::main]
 async fn main() {
 
@@ -41,12 +79,11 @@ async fn main() {
     dotenv().ok();
 
     let opts = CorralWebserverOpts::from_args();
-
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async {
-        info!("Some logging");
-        "Hello, World!"
-    }));
+    let app = Router::new().route(
+        "/",
+        get(handler)
+    );
 
     // run it with hyper on localhost:3000
     let uri = format!("{}:{}", opts.host, opts.port);
